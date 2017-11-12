f3c8169d347f5914430425da262d9093a65e1d53
Robert Blutner <robert@blutner.de>
2017-11-12 01:53:40 +0100

    Basic jwt implementation

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const Hapi = require("hapi")
 const Routes = require("./lib/routes")
 
-const server = new Hapi.Server
-
-server.connection({
-    port: 3000
+const server = new Hapi.Server({
+    port: 3001
 })
 
-server.route(Routes)
+const provision = async () => {
+    server.route(Routes)
 
-server.start((error) => {
-    if(error) {
-        throw error
-    }
+    server.events.on("log", (event, tags) => {
+        console.log(event)
+    })
 
+    await server.start()
+}
+
+provision().then(() => {
     console.log("Ocomis authentication service started.")
     console.log("Server running at: " + server.info.uri)
+}).catch((error) => {
+    console.error(error)
 })
